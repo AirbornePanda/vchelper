54a656f80ff674cf7ce170ca8bf798015e2a9cc4
Julian Sauer <julian.sauer@outlook.com>
2018-09-10 19:29:36 +0200

    Add repository annotation to hero repository.

--- src/main/java/de/jsauer/spring/backend/repositories/HeroRepository.java
+++ src/main/java/de/jsauer/spring/backend/repositories/HeroRepository.java
@@ -3,9 +3,11 @@ package de.jsauer.spring.backend.repositories;
 import de.jsauer.spring.backend.entities.Hero;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface HeroRepository extends JpaRepository<Hero, Long> {
 
     Hero findOneByGarmId(String garmId);
